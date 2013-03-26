c7cf6c956835d2d0f5cfdfe6af954f0a1c1364d2
ladysnow <malin@randstrom.com>
2013-03-26 23:16:12 +0100

    new directory with concrete class for libreoffice specifics and an astract class/interface SpreadSheet

---
+++ posanal/spreadsheet/libreoffice.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append('/usr/local/lib/python3.3/dist-packages')
+
+from .base import SpreadSheet
+
+import uno
+import unohelper
+from com.sun.star.script.provider import XScriptProviderFactory
+from com.sun.star.script.provider import XScriptProvider
+from com.sun.star.script.provider import XScriptContext 
+from com.sun.star.lang import Locale as sLocale
+
+class LibreOffice(SpreadSheet):
+    def __init__(self,oDoc):
+        self.oDoc = oDoc
+        self.libreSheet = self.oDoc.CurrentController.getActiveSheet()
+        self.cell = 0
+
+    def setCell(self,x,y):
+        self.cell = self.libresheet.getCellByPosition(x,y)
+
+    def setFloat(self,value):
+        self.cell.setValue(float(value))
+
+    def setInt(self,value):
+        self.cell.setValue(int(value))                             
+
+    def setValue(self,value):
+        self.cell.setValue(value)
+
+    def setString(self,value):
+        self.setString(value)
+        
+    def setFormula(self,value):
+        self.cell.setFormula(value)
+
+    def setCellBackColor(self,color):
+        self.cell.CellBackColor = color 
+
+    def setHoriJustify(self,j):
+        self.cell.HoriJustify = uno.getConstantByName("com.sun.star.awt.TextAlign." + j)
+
+    def setFontWeight(self, w):
+        self.cell.CharWeight = uno.getConstantByName("com.sun.star.awt.FontWeight." + w )
+
+    def setFormat(self,format):
+        self.cell.NumberFormat = self.getFormatKey(format)
+
+    def getFormatKey(self,format):
+        NumForms = self.oDoc.getNumberFormats()
+        key = NumForms.queryKey(format, sLocale() , True)
+        if (key == -1):
+            return self.oDoc.NumberFormats.addNew(format,sLocale())
+        return key    
+
+def test():
+    print("test")
--- posanal/utils/spreadsheet.py
+++ posanal/utils/spreadsheet.py
@@ -1,41 +1,63 @@
 '''
 Created on 22 Mar 2013
 
+The purpose of the abstract class is to be able to test outside libreoffice
+
 @author: malin
 '''
+from abc import abstractmethod 
 import sys
+
 sys.path.append('/usr/local/lib/python3.3/dist-packages')
 
-class SpreadSheet:
+class SpreadSheet(object):
     def __init__(self,oDoc):
         self.oDoc = oDoc
 
+    @abstractmethod
     def setValue(self,value):
         print ("setValue " + str(value))
-
+        pass
+        
+    @abstractmethod
     def setString(self,value):
         print ("setString " + value)
-        
+        pass
+      
+    @abstractmethod
     def setFormula(self,value):
         print ("setFormula " + str(value))
+        pass
 
+    @abstractmethod
     def setCellBackColor(self,color):
         print ("setCellBackColor " + str(color))
-
+        pass
+    
+    @abstractmethod
     def setHoriJustify(self,j):
         print ("HoriJustify " + j)
-
+        pass
+    
+    @abstractmethod
     def setCharWeight(self, w):
-        print("setWeight " +w )
-
+        print("setWeight " + w)
+        pass
+    
+    @abstractmethod
     def setFormat(self, format):
         print("setFormat " + str(format))
-
+        pass
+ 
+    @abstractmethod
     def getFormatKey(self,format):
         return 1
-    
+        pass
+
+    @abstractmethod    
     def setCell(self,x,y):
         print("setCell " + str(x) +" " + str(y))
-
+        pass
+    
 def test():
     print("test")
